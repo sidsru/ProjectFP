88339b66485bae03a25f9f21294cc1596993832a
ParkChanYeong <sidsru21131210@gmail.com>
2026-04-03 12:11:24 +0900

    스팀 연동 친구 초대기능 추가

--- Source/ProjectFP/Private/GameMode/FPGameInstance.cpp
+++ Source/ProjectFP/Private/GameMode/FPGameInstance.cpp
@@ -9,103 +9,107 @@
 
 void UFPGameInstance::CreateSession()
 {
-    IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
-    if (!Subsystem) return;
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (!Subsystem) return;
 
-    IOnlineSessionPtr Session = Subsystem->GetSessionInterface();
-    if (!Session.IsValid()) return;
+	IOnlineSessionPtr Session = Subsystem->GetSessionInterface();
+	if (!Session.IsValid()) return;
 
-    FOnlineSessionSettings Settings;
-    Settings.bIsLANMatch = false;
-    Settings.NumPublicConnections = 4;
-    Settings.bAllowJoinInProgress = true;
+	FOnlineSessionSettings Settings;
+	Settings.bIsLANMatch = false;
+	Settings.NumPublicConnections = 4;
+	Settings.bAllowJoinInProgress = true;
 
-    Settings.bUsesPresence = true;
-    Settings.bUseLobbiesIfAvailable = true;
+	Settings.bUsesPresence = true;
+	Settings.bUseLobbiesIfAvailable = true;
 
-    Session->AddOnCreateSessionCompleteDelegate_Handle(
-        FOnCreateSessionCompleteDelegate::CreateUObject(
-            this,
-            &UFPGameInstance::OnCreateSessionComplete
-        )
-    );
+	Session->AddOnCreateSessionCompleteDelegate_Handle(
+		FOnCreateSessionCompleteDelegate::CreateUObject(
+			this,
+			&UFPGameInstance::OnCreateSessionComplete
+		)
+	);
 
-    Session->CreateSession(0, NAME_GameSession, Settings);
+	Session->CreateSession(0, NAME_GameSession, Settings);
+	UE_LOG(LogTemp, Warning, TEXT("CreateSession called"));
 }
 
 void UFPGameInstance::InviteFriend()
 {
-    IOnlineExternalUIPtr ExternalUI = Online::GetExternalUIInterface(GetWorld());
+	IOnlineExternalUIPtr ExternalUI = Online::GetExternalUIInterface(GetWorld());
 
-    if (ExternalUI.IsValid())
-    {
-        ExternalUI->ShowInviteUI(0, NAME_GameSession);
-    }
+	if (ExternalUI.IsValid())
+	{
+		ExternalUI->ShowInviteUI(0, NAME_GameSession);
+	}
 }
 
 void UFPGameInstance::Init()
 {
-    Super::Init();
+	Super::Init();
+	UE_LOG(LogTemp, Warning, TEXT("Init called"));
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (!Subsystem) return;
 
-    IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
-    if (!Subsystem) return;
+	IOnlineSessionPtr Session = Subsystem->GetSessionInterface();
+	if (!Session.IsValid()) return;
 
-    IOnlineSessionPtr Session = Subsystem->GetSessionInterface();
-    if (!Session.IsValid()) return;
-
-    Session->AddOnSessionUserInviteAcceptedDelegate_Handle(
-        FOnSessionUserInviteAcceptedDelegate::CreateUObject(
-            this,
-            &UFPGameInstance::OnInviteAccepted
-        )
-    );
+	Session->AddOnSessionUserInviteAcceptedDelegate_Handle(
+		FOnSessionUserInviteAcceptedDelegate::CreateUObject(
+			this,
+			&UFPGameInstance::OnInviteAccepted
+		)
+	);
+	CreateSession();
 }
 
 void UFPGameInstance::OnCreateSessionComplete(FName SessionName, bool bSuccess)
 {
-    if (bSuccess)
-    {
-        GetWorld()->ServerTravel("/Game/Map/TestLevel?listen");
-    }
+	UE_LOG(LogTemp, Warning, TEXT("Session Created: %d"), bSuccess);
+	if (bSuccess)
+	{
+		GetWorld()->ServerTravel("/Game/Map/TestLevel?listen");
+		InviteFriend();
+	}
 }
 
 void UFPGameInstance::OnInviteAccepted(bool bWasSuccessful, int32 ControllerId, TSharedPtr<const FUniqueNetId> UserId, const FOnlineSessionSearchResult& InviteResult)
 {
-    if (!bWasSuccessful) return;
+	if (!bWasSuccessful) return;
 
-    IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
-    if (!Subsystem) return;
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (!Subsystem) return;
 
-    IOnlineSessionPtr Session = Subsystem->GetSessionInterface();
-    if (!Session.IsValid()) return;
+	IOnlineSessionPtr Session = Subsystem->GetSessionInterface();
+	if (!Session.IsValid()) return;
 
-    Session->AddOnJoinSessionCompleteDelegate_Handle(
-        FOnJoinSessionCompleteDelegate::CreateUObject(
-            this,
-            &UFPGameInstance::OnJoinSessionComplete
-        )
-    );
+	Session->AddOnJoinSessionCompleteDelegate_Handle(
+		FOnJoinSessionCompleteDelegate::CreateUObject(
+			this,
+			&UFPGameInstance::OnJoinSessionComplete
+		)
+	);
 
-    Session->JoinSession(0, NAME_GameSession, InviteResult);
+	Session->JoinSession(0, NAME_GameSession, InviteResult);
 }
 
 void UFPGameInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
 {
-    IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
-    if (!Subsystem) return;
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (!Subsystem) return;
 
-    IOnlineSessionPtr Session = Subsystem->GetSessionInterface();
-    if (!Session.IsValid()) return;
+	IOnlineSessionPtr Session = Subsystem->GetSessionInterface();
+	if (!Session.IsValid()) return;
 
-    FString ConnectString;
+	FString ConnectString;
 
-    if (Session->GetResolvedConnectString(SessionName, ConnectString))
-    {
-        APlayerController* PC = GetFirstLocalPlayerController();
+	if (Session->GetResolvedConnectString(SessionName, ConnectString))
+	{
+		APlayerController* PC = GetFirstLocalPlayerController();
 
-        if (PC)
-        {
-            PC->ClientTravel(ConnectString, TRAVEL_Absolute);
-        }
-    }
+		if (PC)
+		{
+			PC->ClientTravel(ConnectString, TRAVEL_Absolute);
+		}
+	}
 }
